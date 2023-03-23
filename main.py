819137bc500bf6ac29f1e4626616ba4c5bd28571
ZARIFMAC <uzbzarif@gmail.com>
2023-03-23 18:35:35 +0500

    waitKey

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ img = np.zeros((512, 512, 3), np.uint8)
 cv2.circle(img, (256, 256), 100, (0, 0, 255), 2)
 cv2.imshow("Image", img)
 # Wait for any key to be pressed
-cv2.waitKey(0)
+while True:
+    # 0 means wait for any key to be pressed
+    # 1 means wait for 1 millisecond
+    k = cv2.waitKey(0) 
+    print(k)
+    if k == 27:
+        break
+
 # Close all windows
 cv2.destroyAllWindows()
