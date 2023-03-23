83dad88ea03c75da26b826206cfe6f2a650b3afb
ZARIFMAC <uzbzarif@gmail.com>
2023-03-23 19:59:15 +0500

    fix

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import cv2
 import numpy as np
 # Print the version of OpenCV
-print(cv2.__version__)
 
-img = np.zeros((512, 512, 3), np.uint8)
-# Draw a circle to the center of the image
-cv2.circle(img, (256, 256), 100, (0, 0, 255), 2)
-cv2.imshow("Image", img)
+
+
 # Wait for any key to be pressed
+img = np.zeros((512, 512, 3), np.uint8)
+cv2.imshow("Image", img)
+# Define a variable for radius
+R = 10
 while True:
     # 0 means wait for any key to be pressed
     # 1 means wait for 1 millisecond
     k = cv2.waitKey(0) 
-    print(k)
+    if k==ord('+'):
+        R += 1
+    elif k==ord('-'):
+        R -= 1
+
+
+    
+    
+    # Draw a circle to the center of the image
+    img = np.zeros((512, 512, 3), np.uint8)
+    cv2.circle(img, (256, 256), R, (0, 0, 255), 2)
+    
+    cv2.imshow("Image", img)
+    
     if k == 27:
         break
 
